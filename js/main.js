94ccf3096f11b8217516051554c219a3377e9254
Adeen Shukla <adeens357@gmail.com>
2018-03-19 22:31:32 +0530

    Implement basic functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+let quotes = ["Hello There", "General Kenobi, you are a bold one", "I have the high ground", "It's treason then"];
 document.getElementById('getQuoteBtn').onclick = function() {
-        alert("Button Clicked");
+    var random = quotes[Math.floor(Math.random()*quotes.length)];
+    alert(random);
 };
